650b27f9bb0b38ac047657a4d3d9ebca8c0b29b6
kazuya kawaguchi <kawakazu80@gmail.com>
2020-08-14 10:57:34 +0900

    update

--- external.js
+++ external.js
@@ -3,5 +3,11 @@ module.exports = ({ github, context, core, io }, data) => {
   console.log('j', j)
   console.log('data type', typeof data)
   console.log('data', data)
+  github.issues.create({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    title: 'test test',
+    body: 'this is test'
+  })
   return context.payload
 }
