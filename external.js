451713db5a18858894d5e27dd7e6d753b1e0eb4f
kazuya kawaguchi <kawakazu80@gmail.com>
2020-08-14 10:49:12 +0900

    update

--- external.js
+++ external.js
@@ -1,4 +1,6 @@
 module.exports = ({ github, context, core, io }, data) => {
+  const j = require('./package.json')
+  console.log('j', j)
   console.log('data type', typeof data)
   console.log('data', data)
   return context.payload
