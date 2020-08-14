355ef8ed6e9f7784efa918bfd29dcacad163edea
kazuya kawaguchi <kawakazu80@gmail.com>
2020-08-14 15:35:37 +0900

    update

--- external.js
+++ external.js
@@ -1,13 +1,14 @@
 module.exports = ({ github, context, core, io }, data) => {
-  const j = require('./package.json')
-  console.log('j', j)
-  console.log('data type', typeof data)
-  console.log('data', data)
-  github.issues.create({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    title: 'test test',
-    body: 'this is test'
-  })
-  return context.payload
+  // const j = require('./package.json')
+  // console.log('j', j)
+  // console.log('data type', typeof data)
+  // console.log('data', data)
+  // github.issues.create({
+  //   owner: context.repo.owner,
+  //   repo: context.repo.repo,
+  //   title: 'test test',
+  //   body: 'this is test'
+  // })
+  console.log('github', github)
+  return context
 }
