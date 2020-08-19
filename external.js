2f574dd80d5094a8d161b7c728eba58cbe466794
kazuya kawaguchi <kawakazu80@gmail.com>
2020-08-19 20:11:23 +0900

    update

--- external.js
+++ external.js
@@ -18,6 +18,7 @@ module.exports = async ({ github, context, core, io }) => {
       try {
         const filePath = d.file
         const blameRes = await getBlame(github, context.repo.repo, context.repo.owner, branch, filePath)
+        console.log('getBlane', blameRes)
         for (const message of d.messages) {
           const comment = createComment(blameRes.data, filePath, message, blob)
           console.log('comment', comment)
