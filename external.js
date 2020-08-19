87bb0277ffb6ec2975bbb3af7b631cfa23ecea45
kazuya kawaguchi <kawakazu80@gmail.com>
2020-08-19 20:27:27 +0900

    update

--- external.js
+++ external.js
@@ -6,70 +6,33 @@ module.exports = async ({ github, context, core, io }) => {
   const reports = createReports(blob, data);
   for (const r of reports) {
     console.log(`register issue for ${r.package}, ${[...r.body].length}, ${r.body.length} ...`);
-    // const issueRes = await github.issues.create({
-    //   owner: context.repo.owner,
-    //   repo: context.repo.repo,
-    //   title: r.title,
-    //   body: r.body
-    // })
+    const issueRes = await github.issues.create({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      title: r.title,
+      body: r.body
+    })
     // console.log('issue', issueRes)
     for (const d of r.details) {
-      // console.log('detail', d)
       try {
         const filePath = d.file
         const blameRes = await getBlame(github, context.repo.repo, context.repo.owner, branch, `packages/${filePath}`)
-        console.log('brameRes', JSON.stringify(blameRes))
+        // console.log('brameRes', JSON.stringify(blameRes))
         for (const message of d.messages) {
           const comment = createComment(blameRes, filePath, message, blob)
-          console.log('comment', comment)
+          // console.log('comment', comment)
+          await github.issues.createComment({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            issue_number: issueRes.data.number,
+            body: comment
+          })
         }
       } catch (e) {
         console.error(e)
       }
-      // await github.issues.createComment({
-      //   owner: context.repo.owner,
-      //   repo: context.repo.repo,
-      //   issue_number: issueRes.data.number,
-      //   body: d
-      // })
     }
   }
-  // console.log('github', github)
-  /*
-  const res = await github.graphql(
-`
-  {
-    repository(owner: "kazupon", name: "vue-i18n") {
-      ref(qualifiedName:"master") {  
-        name
-        target {
-          ... on Commit {
-            author {
-              name
-            }
-            blame(path:"src/index.js") {
-              ranges {
-                commit {
-                  author {
-                    name
-                  }
-                }
-                startingLine
-                endingLine
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`, {}, {
-  headers: {
-    authorization: process.env.GITHUB_TOKEN
-  }
-})
-  console.log('res', JSON.stringify(res))
-  */
   return context
 }
 
@@ -85,8 +48,8 @@ https://github.com/kazupon/sandbox-github-actions/blob/${blob}/packages/${filePa
 }
 
 function getUser(ranges, message) {
-  console.log('getUser: ranges', ranges)
-  console.log('getUser: message', message)
+  // console.log('getUser: ranges', ranges)
+  // console.log('getUser: message', message)
   let user = null
   for (const range of ranges) {
     if (message.line >= range.startingLine || message.line <= range.endingLine) {
@@ -98,7 +61,6 @@ function getUser(ranges, message) {
 }
 
 function getBlame(github, repo, owner, branch, path) {
-  console.log('getblame', repo, owner, branch, path)
   return github.graphql(
     `
       {
